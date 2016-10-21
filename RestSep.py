3df9a36d4174c7bbe0e380ce3209f7e33969f9ab
Yang Luo <hsluoyz@qq.com>
2016-10-21 16:58:20 +0800

    Added init_random_matrix().

--- RestSep.py
+++ RestSep.py
@@ -3,11 +3,12 @@
 import re
 from pprint import pprint
 import numpy as np
+import random
 
 
 test_matrix = {}
 api_set = set()
-api_array = []
+api_list = []
 
 def method_path_to_string(method, path):
     return path + " | " + method
@@ -115,14 +116,38 @@ def init_from_test():
             api_set.add(method_path_to_string(method, path))
 
 
-def print_matrix(matrix):
-    x_count, y_count = matrix.shape
+def print_matrix(m):
+    row_size, col_size = m.shape
     res = ""
-    for y in range(0, y_count):
-        for x in range(x_count):
-            res += str(matrix[x, y])
+    for i in range(0, row_size):
+        for j in range(0, col_size):
+            res += str(m[i, j])
         res += '\n'
     print res
+
+
+def init_random_matrix(row_size, col_size):
+    m = np.zeros((row_size, col_size), dtype=np.int)
+    for j in range(0, col_size):
+        m[random.randrange(0, row_size, 1), j] = 1
+    return m
+
+
+def print_result_from_matrix(m):
+    row_size, col_size = m.shape
+    res = []
+
+    for i in range(0, row_size):
+        row_api_list = []
+        for j in range(0, col_size):
+            if m[i, j] == 1:
+                row_api_list.append(api_list[j])
+        if len(row_api_list) != 0:
+            res.append(row_api_list)
+
+    pprint(res)
+    print "Category number = " + str(len(res))
+
 
 init_from_test()
 
@@ -131,22 +156,23 @@ pprint(test_matrix)
 
 # Print the APIs.
 
-api_array = list(api_set)
-api_array.sort()
+api_list = list(api_set)
+api_list.sort()
 
-for i in range(0, len(api_array)):
-    print str(i) + ":\t" + api_array[i]
+for i in range(0, len(api_list)):
+    print str(i) + ":\t" + api_list[i]
     i += 1
 
-api_count = len(api_array)
+api_count = len(api_list)
 
 print "Test case number = " + str(len(test_matrix))
 print "API number = " + str(api_count)
 
-# matrix_np = np.array(matrix)
+m = init_random_matrix(api_count / 3, api_count)
 
-m = np.ones((api_count, api_count), dtype=np.int)
 print_matrix(m)
+
+# print_result_from_matrix(m)
 
 # a = [1, 32]
 # b = [3, 4]
